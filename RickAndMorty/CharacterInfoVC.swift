a298b8c4f3b8df4c20163402eb4faba23731fc6e
Egopoler <egpol2004@yandex.ru>
2023-07-02 13:43:25 +0300

    add character info loading

--- RickAndMorty/CharacterInfoVC.swift
+++ RickAndMorty/CharacterInfoVC.swift
@@ -8,11 +8,46 @@
 import UIKit
 
 class CharacterInfoVC: UIViewController {
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
     var character: Character?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let realCharacter = character ?? Character(id: -1, name: "Anonim", status: Character.Status.unknown, species: "unknown", gender: Character.Gender.unknown, location: "unknowm", image: "")
+        
         // Do any additional setup after loading the view.
+        
+        nameTextField.text = realCharacter.name
+        
+        switch realCharacter.gender {
+        case .male:
+            genderLabel.text = "Male"
+        case .female:
+            genderLabel.text = "Female"
+        case .genderless:
+            genderLabel.text = "Genderless"
+        case .unknown:
+            genderLabel.text = "Unknown"
+        }
+        
+        locationTextField.text = realCharacter.location
+        
+        switch realCharacter.status {
+        case .alive:
+            statusLabel.text = "Alive"
+        case .dead:
+            statusLabel.text = "Dead"
+        case .unknown:
+            statusLabel.text = "Unknown"
+        }
+        
+        speciesLabel.text = realCharacter.species
+        
+        
     }
     
 
